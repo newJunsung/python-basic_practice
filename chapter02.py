fab949e4f7799d3f8bf83e6cfb782bc1f4618986
newJunsung <assd1108@naver.com>
2022-06-25 21:54:14 +0900

    영상 2장 연습(실습)

---
+++ chapter02.py
@@ -0,0 +1,21 @@
+# name = "홍길동"
+# age = 19
+# is_adult = age > 19
+
+# # '+'가 들어가면 print 내의 모든 변수는 string으로 형변환 해야 됨.
+# print("내 이름은 " + name + "입니다.")
+# print("나이는 " + str(age) + "살입니다.")
+# print(name + "은 성인입니까?>> " + str(is_adult))
+
+# #반면에 ','가 들어가면 print 내의 모든 변수를 형번환 할 필요 없음.
+# print("내 이름은 ", name, "입니다.")
+# print("나이는 ", age, "살입니다.")
+# print(name, "은 성인입니까?>> ", is_adult)
+
+# Quiz
+station = "사당"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "신도림"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "인천공항"
+print(station + "행 열차가 들어오고 있습니다.")
